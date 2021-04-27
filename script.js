c94e7adf37d7e2968965cfc86ffb044683305957
Dat Nguyen <nguyentiendat5699@gmail.com>
2021-04-27 14:59:40 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+function pizzaOven(crustType, sauceType, cheeses, toppings){
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+var pizza = pizzaOven("deep dish", "traditional", ["mozzarella"], ["peperoni", "sausage"])
+console.log(pizza);
+
+// 2
+function pizzaOven(crustType, sauceType, cheeses, toppings){
+    var p2 = {};
+    p2.crustType = crustType;
+    p2.sauceType = sauceType;
+    p2.cheeses = cheeses;
+    p2.toppings = toppings;
+    return p2;
+}
+var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushroom", "olives", "onions"])
+console.log(p2);
+
+// 3
+function pizzaOven(crustType, sauceType, cheeses, toppings){
+    var p3 = {};
+    p3.crustType = crustType;
+    p3.sauceType = sauceType;
+    p3.cheeses = cheeses;
+    p3.toppings = toppings;
+    return p3;
+}
+var p3 = pizzaOven("brooklyn style", "honey bbq sauce", ["mozzarella", "cheddar"], ["mushroom", "ham", "bacon"])
+console.log(p3);
+
+// 4
+function pizzaOven(crustType, sauceType, cheeses, toppings){
+    var p4 = {};
+    p4.crustType = crustType;
+    p4.sauceType = sauceType;
+    p4.cheeses = cheeses;
+    p4.toppings = toppings;
+    return p4;
+}
+var p4 = pizzaOven("hand tossed", "ranch", ["cheddar"], ["beef", "italian sausage"])
+console.log(p4);
+
+
